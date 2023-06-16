4bce1f7f2a8d890c2dfd291d84f1fee39bb5db41
sujang.958 <enter7377@naver.com>
2023-06-16 22:11:33 +0900

    docs: create README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# N Word Counter
+
+> A Discord bot that counts the N word
+
+[Invite](https://discord.com/api/oauth2/authorize?client_id=1118903489423228998&permissions=67584&scope=bot)
+
+---
+
+_Made with ❤️ by sujang958_
--- src/app.ts
+++ src/app.ts
@@ -54,8 +54,6 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
 client.login(process.env.BOT_TOKEN)
 
-// https://discord.com/api/oauth2/authorize?client_id=1118903489423228998&permissions=67584&scope=bot
-
 process.on("uncaughtException", (error) => {
   console.log("Error", error)
 })
