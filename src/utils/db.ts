cf6879328215071bebddacf043068ba57c41c858
sujang.958 <enter7377@naver.com>
2023-06-25 16:37:13 +0900

    ref: finish migration

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,26 +4,13 @@ import { z } from "zod"
 import { PlanetScaleDatabase } from "drizzle-orm/planetscale-serverless"
 import { User, users } from "../db/schema"
 import { eq } from "drizzle-orm"
+import admin from "firebase-admin"
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(require("../../account.json")),
-// })
+admin.initializeApp({
+  credential: admin.credential.cert(require("../../account.json")),
+})
 
-// export const userCollection = admin.firestore().collection("users")
-
-// export const getOrCreate = async (id: string) => {
-//   const userDocRef = userCollection.doc(id)
-//   const userDoc = await userDocRef.get()
-//   if (userDoc.exists) return userDocRef
-
-//   await userDocRef.create({
-//     KRN: 0,
-//     N: 0,
-//     createdAt: Date.now(),
-//   })
-
-//   return userDocRef
-// }
+export const userCollection = admin.firestore().collection("users")
 
 export const selectOrInsert = async (
   db: PlanetScaleDatabase<Record<string, never>>,
