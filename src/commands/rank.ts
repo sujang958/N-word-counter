ae28cf0757ed9da561acb00f3898e0e9904c2519
sujang.958 <enter7377@naver.com>
2023-06-25 16:26:01 +0900

    feat: update leaderboard command

--- src/commands/rank.ts
+++ src/commands/rank.ts
@@ -1,48 +1,39 @@
-// import {
-//   CommandInteraction,
-//   EmbedBuilder,
-//   GuildMember,
-//   SlashCommandBuilder,
-// } from "discord.js"
-// import { CommandFile } from "../types"
-// import { getOrCreate, userCollection, userDoc, userDocType } from "../utils/db"
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js"
+import { CommandFile } from "../types"
+import { users } from "../db/schema"
+import { desc } from "drizzle-orm"
 
-// const RankCommand: CommandFile = {
-//   data: new SlashCommandBuilder()
-//     .setName("leaderboard")
-//     .setDescription("See who's the greatest racist")
-//     .toJSON(),
-//   async execute(interaction, client) {
-//     const snapshot = await userCollection.get()
-//     const users = (
-//       snapshot.docs
-//         .filter((doc) => doc.exists)
-//         .map((user) => ({ ...user.data(), id: user.id }))
-//         .filter((user) => userDocType.safeParse(user).success) as userDoc[]
-//     )
-//       .sort((a, b) => a.KRN + a.N - (b.KRN + b.N))
-//       .reverse()
-//       .slice(0, 5)
+const RankCommand: CommandFile = {
+  data: new SlashCommandBuilder()
+    .setName("leaderboard")
+    .setDescription("See who's the greatest racist")
+    .toJSON(),
+  async execute(interaction, client, db) {
+    const fetchedUsers = await db
+      .select()
+      .from(users)
+      .orderBy(desc(users.counts))
+      .limit(10)
 
-//     const embed = new EmbedBuilder()
-//       .setTitle("Hall of Racism")
-//       .setColor(0x2a1f1c)
-//       .setTimestamp(new Date())
+    const embed = new EmbedBuilder()
+      .setTitle("Hall of Racism")
+      .setColor(0x2a1f1c)
+      .setTimestamp(new Date())
 
-//     for (const i in users) {
-//       const user = users[i]
-//       const discordUser = await client.users.fetch(user.id)
-//       const counts = (user.KRN + user.N).toLocaleString()
+    for (const i in fetchedUsers) {
+      const user = fetchedUsers[i]
+      const discordUser = await client.users.fetch(user.discordId)
+      const counts = user.counts.toLocaleString()
 
-//       embed.addFields({
-//         name: `${parseInt(i) + 1}. ${discordUser.username}`,
-//         value: `${counts} times`,
-//         inline: false,
-//       })
-//     }
+      embed.addFields({
+        name: `${parseInt(i) + 1}. ${discordUser.username}`,
+        value: `${counts} times`,
+        inline: false,
+      })
+    }
 
-//     interaction.reply({ embeds: [embed] })
-//   },
-// }
+    interaction.reply({ embeds: [embed] })
+  },
+}
 
-// export default RankCommand
+export default RankCommand
